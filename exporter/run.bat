193086c7735602b2b6e49f324e87952418d9d8cb
Danny Angelo Carminati Grein <fungos@gmail.com>
2010-04-19 11:51:16 -0300

    ! Fix env SEEDSDK

--- exporter/run.bat
+++ exporter/run.bat
@@ -2,11 +2,11 @@
 rem del cache.sdl.txt
 del StringCache.txt
 sexport -i sdl.xml -p sdl -v
-copy sdl.h %TF_BASE%\SDK\demos\common\sdl\assets.h
+copy sdl.h %SEEDSDK%\seeddemos\common\sdl\assets.h
 @echo Building WII demo files
 rem del cache.wii.txt
 rem del StringCache.txt
 rem sexport -i wii.xml -p wii -v
-rem copy wii.h %TF_BASE%\SDK\demos\common\wii\assets.h
+rem copy wii.h %SEEDSDK%\seeddemos\common\wii\assets.h
 @echo Done!
 pause
